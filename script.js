d7418615ec1491cd0f73270dc5d1c20d609d70ed
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-11-20 14:21:36 +0100

    Stackblitz slettet forrige.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
+myFunction(fname, lname)
+{
+const date=new Date()
+const timestring=date.toLocaleTimeString();
+document.getElementById("showtime")+timestring;
 
-//function showDateTime(fname,lname) {const current = new Date(); const datestring = current.toLocaleString();document.getElementById('showtime').textContent = "Current Date and Time: " + datestring;}//*
 
-function myFunction(fname,lname){
-    const date=new Date();
-    const timestring=date.toLocaleString();
-
-    document.getElementById('showtime').textContent= "Current time and date" + timestring;
 }
 
+console.log(myFunction)
